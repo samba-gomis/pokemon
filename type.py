0c5fb42d839b1c5dee8a5b97dc68304035cc4273
mahira-manico <mahira.manico@laplateforme.io>
2026-02-11 13:01:10 +0100

    feat: add of type file for pokemon types

---
+++ type.py
@@ -0,0 +1,109 @@
+TYPE_DAMAGE = {
+    "Normal": {
+        "Normal": 1, "Fire": 0.75, "Water": 0.75, "Electric": 0.75, "Grass": 0.75, 
+        "Ice": 0.75, "Fighting": 0.75, "Poison": 0.75, "Ground": 0.75, "Flying": 0.75, 
+        "Psychic": 0.75, "Bug": 0.75, "Rock": 0.5, "Ghost": 0, "Dragon": 0.75, 
+        "Dark": 0.75, "Steel": 0.5, "Fairy": 0.75},
+    
+    "Fire": {
+        "Normal": 1, "Fire": 0.5, "Water": 0.5, "Electric": 0.75, "Grass": 2, 
+        "Ice": 2, "Fighting": 0.75, "Poison": 0.75, "Ground": 0.75, "Flying": 0.75, 
+        "Psychic": 0.75, "Bug": 2, "Rock": 0.5, "Ghost": 0.75, "Dragon": 0.5, 
+        "Dark": 0.75, "Steel": 2, "Fairy": 0.75},
+    
+    "Water": {
+        "Normal": 1, "Fire": 2, "Water": 0.5, "Electric": 0.75, "Grass": 0.5, 
+        "Ice": 0.75, "Fighting": 0.75, "Poison": 0.75, "Ground": 2, "Flying": 0.75, 
+        "Psychic": 0.75, "Bug": 0.75, "Rock": 2, "Ghost": 0.75, "Dragon": 0.5, 
+        "Dark": 0.75, "Steel": 0.75, "Fairy": 0.75},
+    
+    "Electric": {
+        "Normal": 1, "Fire": 0.75, "Water": 2, "Electric": 0.5, "Grass": 0.5, 
+        "Ice": 0.75, "Fighting": 0.75, "Poison": 0.75, "Ground": 0, "Flying": 2, 
+        "Psychic": 0.75, "Bug": 0.75, "Rock": 0.75, "Ghost": 0.75, "Dragon": 0.5, 
+        "Dark": 0.75, "Steel": 0.75, "Fairy": 0.75},
+    
+    "Grass": {
+        "Normal": 1, "Fire": 0.5, "Water": 2, "Electric": 0.75, "Grass": 0.5, 
+        "Ice": 0.75, "Fighting": 0.75, "Poison": 0.5, "Ground": 2, "Flying": 0.5, 
+        "Psychic": 0.75, "Bug": 0.5, "Rock": 2, "Ghost": 0.75, "Dragon": 0.5, 
+        "Dark": 0.75, "Steel": 0.5, "Fairy": 0.75},
+    
+    "Ice": {
+        "Normal": 1, "Fire": 0.5, "Water": 0.5, "Electric": 0.75, "Grass": 2, 
+        "Ice": 0.5, "Fighting": 0.75, "Poison": 0.75, "Ground": 2, "Flying": 2, 
+        "Psychic": 0.75, "Bug": 0.75, "Rock": 0.75, "Ghost": 0.75, "Dragon": 2, 
+        "Dark": 0.75, "Steel": 0.5, "Fairy": 0.75},
+    
+    "Fighting": {
+        "Normal": 2, "Fire": 0.75, "Water": 0.75, "Electric": 0.75, "Grass": 0.75, 
+        "Ice": 2, "Fighting": 0.75, "Poison": 0.5, "Ground": 0.75, "Flying": 0.5, 
+        "Psychic": 0.5, "Bug": 0.5, "Rock": 2, "Ghost": 0, "Dragon": 0.75, 
+        "Dark": 2, "Steel": 2, "Fairy": 0.5},
+    
+    "Poison": {
+        "Normal": 1, "Fire": 0.75, "Water": 0.75, "Electric": 0.75, "Grass": 2, 
+        "Ice": 0.75, "Fighting": 0.75, "Poison": 0.5, "Ground": 0.5, "Flying": 0.75, 
+        "Psychic": 0.75, "Bug": 0.75, "Rock": 0.5, "Ghost": 0.5, "Dragon": 0.75, 
+        "Dark": 0.75, "Steel": 0, "Fairy": 2},
+    
+    "Ground": {
+        "Normal": 1, "Fire": 2, "Water": 0.75, "Electric": 2, "Grass": 0.5, 
+        "Ice": 0.75, "Fighting": 0.75, "Poison": 2, "Ground": 0.75, "Flying": 0, 
+        "Psychic": 0.75, "Bug": 0.5, "Rock": 2, "Ghost": 0.75, "Dragon": 0.75, 
+        "Dark": 0.75, "Steel": 2, "Fairy": 0.75},
+    
+    "Flying": {
+        "Normal": 1, "Fire": 0.75, "Water": 0.75, "Electric": 0.5, "Grass": 2, 
+        "Ice": 0.75, "Fighting": 2, "Poison": 0.75, "Ground": 0.75, "Flying": 0.75, 
+        "Psychic": 0.75, "Bug": 2, "Rock": 0.5, "Ghost": 0.75, "Dragon": 0.75, 
+        "Dark": 0.75, "Steel": 0.5, "Fairy": 0.75},
+    
+    "Psychic": {
+        "Normal": 1, "Fire": 0.75, "Water": 0.75, "Electric": 0.75, "Grass": 0.75, 
+        "Ice": 0.75, "Fighting": 2, "Poison": 2, "Ground": 0.75, "Flying": 0.75, 
+        "Psychic": 0.5, "Bug": 0.75, "Rock": 0.75, "Ghost": 0.75, "Dragon": 0.75, 
+        "Dark": 0, "Steel": 0.5, "Fairy": 0.75},
+    
+    "Bug": {
+        "Normal": 1, "Fire": 0.5, "Water": 0.75, "Electric": 0.75, "Grass": 2, 
+        "Ice": 0.75, "Fighting": 0.5, "Poison": 0.5, "Ground": 0.75, "Flying": 0.5, 
+        "Psychic": 2, "Bug": 0.75, "Rock": 0.75, "Ghost": 0.5, "Dragon": 0.75, 
+        "Dark": 2, "Steel": 0.5, "Fairy": 0.5},
+    
+    "Rock": {
+        "Normal": 1, "Fire": 2, "Water": 0.75, "Electric": 0.75, "Grass": 0.75, 
+        "Ice": 2, "Fighting": 0.5, "Poison": 0.75, "Ground": 0.5, "Flying": 2, 
+        "Psychic": 0.75, "Bug": 2, "Rock": 0.75, "Ghost": 0.75, "Dragon": 0.75, 
+        "Dark": 0.75, "Steel": 0.5, "Fairy": 0.75},
+    
+    "Ghost": {
+        "Normal": 0, "Fire": 0.75, "Water": 0.75, "Electric": 0.75, "Grass": 0.75, 
+        "Ice": 0.75, "Fighting": 0.75, "Poison": 0.75, "Ground": 0.75, "Flying": 0.75, 
+        "Psychic": 2, "Bug": 0.75, "Rock": 0.75, "Ghost": 2, "Dragon": 0.75, 
+        "Dark": 0.5, "Steel": 0.75, "Fairy": 0.75},
+    
+    "Dragon": {
+        "Normal": 1, "Fire": 0.75, "Water": 0.75, "Electric": 0.75, "Grass": 0.75, 
+        "Ice": 0.75, "Fighting": 0.75, "Poison": 0.75, "Ground": 0.75, "Flying": 0.75, 
+        "Psychic": 0.75, "Bug": 0.75, "Rock": 0.75, "Ghost": 0.75, "Dragon": 2, 
+        "Dark": 0.75, "Steel": 0.5, "Fairy": 0},
+    
+    "Dark": {
+        "Normal": 1, "Fire": 0.75, "Water": 0.75, "Electric": 0.75, "Grass": 0.75, 
+        "Ice": 0.75, "Fighting": 0.5, "Poison": 0.75, "Ground": 0.75, "Flying": 0.75, 
+        "Psychic": 2, "Bug": 0.75, "Rock": 0.75, "Ghost": 2, "Dragon": 0.75, 
+        "Dark": 0.5, "Steel": 0.75, "Fairy": 0.5},
+    
+    "Steel": {
+        "Normal": 1, "Fire": 0.5, "Water": 0.5, "Electric": 0.5, "Grass": 0.75, 
+        "Ice": 2, "Fighting": 0.75, "Poison": 0.75, "Ground": 0.75, "Flying": 0.75, 
+        "Psychic": 0.75, "Bug": 0.75, "Rock": 2, "Ghost": 0.75, "Dragon": 0.75, 
+        "Dark": 0.75, "Steel": 0.5, "Fairy": 2},
+    
+    "Fairy": {
+        "Normal": 1, "Fire": 0.5, "Water": 0.75, "Electric": 0.75, "Grass": 0.75, 
+        "Ice": 0.75, "Fighting": 2, "Poison": 0.5, "Ground": 0.75, "Flying": 0.75, 
+        "Psychic": 0.75, "Bug": 0.75, "Rock": 0.75, "Ghost": 0.75, "Dragon": 2, 
+        "Dark": 2, "Steel": 0.5, "Fairy": 0.75}
+}
