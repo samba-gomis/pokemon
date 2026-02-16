dce1943d74a4d1f9cd380a792696302ae0a734a6
moinahalima-abdou <moina-halima.abdou-doukeini@laplateforme.io>
2026-02-16 12:12:45 +0100

    test: test branch test to check

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import pygame
+from graphical_interface import GraphicalInterface
+
+def main():
+    pygame.init()
+    GraphicalInterface()
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
